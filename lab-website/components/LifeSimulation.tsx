ced8ee768ee34edbf3b3c89d8905724ef229854a
Eli Stewart <elistewart@Elis-MacBook-Air.local>
2026-08-31 14:33:33 -0400

    Allow canvas transparency to support alpha-compositing fading

--- lab-website/components/LifeSimulation.tsx
+++ lab-website/components/LifeSimulation.tsx
@@ -23,7 +23,7 @@ export default function LifeSimulation({ className, particleCount, edgeBias = fa
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const ctx = canvas.getContext('2d', { alpha: false }); // alpha:false for optimization if it's the bottom layer
+    const ctx = canvas.getContext('2d');
     if (!ctx) return;
 
     let animationFrameId: number;
